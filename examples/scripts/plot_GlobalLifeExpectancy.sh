65577ad5949acde1d73b802b1fe3c8ba7bb21c2a
Philip Eisenlohr <eisenlohr@egr.msu.edu>
2020-05-06 12:39:33 -0400

    new example of global life expectancy

---
+++ examples/scripts/plot_GlobalLifeExpectancy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT="$( basename "${BASH_SOURCE[0]}" )"
+BASE=$( [[ $SCRIPT =~ plot_(.*)\.sh ]] && echo ${BASH_REMATCH[1]} )
+DATA="$DIR/../data/${BASE}.txt"
+FIGURE="$DIR/../../gallery/${BASE}"
+
+gri -p -output "${FIGURE}.ps" \
+ $GRI/plot \
+ line solid \
+ linewidth 0.5 \
+ symbol dot \
+ color 'rgb 0.45 0.10 0.45' \
+ label outside \
+ aspect 0.5 \
+ frame inch 2 \
+ linear 'year' %g 1750 2050 6 5 1 \
+ log 'life expectancy / a' %g 10 100 2 1 1 \
+ $(columns 'Year' 'LifeExpectancy/a' "$DATA") world \
+
+gri_ps2pdf "${FIGURE}.ps"
